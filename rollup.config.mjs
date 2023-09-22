2f549d3bfadd47f645feeae7438bcc3dcbb686af
Bruce Harrison <leeharrison1984@gmail.com>
2023-09-22 11:07:11 -0500

    add license

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,35 @@
 import ts from 'rollup-plugin-ts';
+import { defineConfig } from 'rollup';
+import copy from 'rollup-plugin-copy';
 
-export default [
-  {
-    input: 'src/bcrypt.ts',
-    output: {
-      dir: 'dist',
-      format: 'esm',
-    },
-    plugins: [
-      ts({
-        cwd: '.',
-        transpiler: 'swc',
-        swcConfig: {
-          jsc: {
-            minify: {
-              compress: true,
-              mangle: true,
-            },
-          },
-          minify: true,
-        },
-      }),
-    ],
+const rollupOptions = defineConfig({
+  input: 'src/bcrypt.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    sourcemap: 'inline',
   },
-];
+  plugins: [
+    copy({
+      targets: [
+        { src: './README.md', dest: './' },
+        { src: './LICENSE', dest: './' },
+      ],
+    }),
+    ts({
+      cwd: '.',
+      transpiler: 'swc',
+      swcConfig: {
+        jsc: {
+          minify: {
+            compress: true,
+            mangle: true,
+          },
+        },
+        minify: true,
+      },
+    }),
+  ],
+});
+
+export default rollupOptions;
